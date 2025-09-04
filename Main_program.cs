0b90853440635d2f46b08c45cb5ca5e6be25fc52
GieziTlax <163209576+GieziTlax@users.noreply.github.com>
2025-09-03 19:24:03 -0600

    Agregar archivos de proyecto.

---
+++ Main_program.cs
@@ -0,0 +1,33 @@
+﻿using Practica_POO_CS;
+using System;
+using System.Collections.Generic;
+
+public class Arch_main
+{
+    public static void Main(string[] args)
+    {
+        //Instanciar objetos /*PESO, ENERGIA, RESISTENCIA, AGILIDAD, IQ, +ATRIBUTO*/
+        Perro firulais = new Perro(25.0, 90, 20.0, 60, 50, 400);
+        Gato michi = new Gato(10.0, 70, 10.0, 80, 90, 150);
+        Humano lucas = new Humano(80.0, 95, 30.0, 85, 115, 120);
+        
+
+        //Acciones
+        firulais.Ladrar();
+        michi.Maullar();
+        lucas.Hablar();
+
+        System.Console.WriteLine($"\nEl Perro corre a una velocidad de {firulais.Correr()} km/h. Salta a una altura de {firulais.Saltar()} m. Y Ataca {firulais.Atacar()} de daño.");
+        System.Console.WriteLine($"El Gato corre a una velocidad de {michi.Correr()} km/h. Salta a una altura de {michi.Saltar()} m. Y Ataca {michi.Atacar()} de daño.");
+        System.Console.WriteLine($"El Humano corre a una velocidad de {lucas.Correr()} km/h. Salta a una altura de {lucas.Saltar()} m. Y Ataca {lucas.Atacar()} de daño. \n");
+        
+        Ser_Vivo[] seres = { firulais, michi, lucas };
+        foreach (Ser_Vivo ser in seres)
+        {
+            ser.Comunicar();
+        }
+
+        //Pausar la consola
+        System.Console.ReadKey();
+    }
+}
---
+++ POO.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO_CS
+{
+    public class Ser_Vivo
+    {
+        //Atributos
+        protected double peso { get; set; }
+        protected int energia { get; set; }
+        protected double resistencia { get; set; }
+        protected int agilidad { get; set; }
+        protected int iq { get; set; }
+
+        //Constructor
+        public Ser_Vivo(double peso, int energia, double resistencia, int agilidad, int iq)
+        {
+            this.peso = peso;
+            this.energia = energia;
+            this.resistencia = resistencia;
+            this.agilidad = agilidad;
+            this.iq = iq;
+        }
+
+        //Metodos
+
+        public virtual void Comunicar()
+        {
+            Console.WriteLine("El ser vivo emite un sonido...");
+        }
+        public double Correr()
+        {
+            double velocidad;
+            return velocidad = ((energia / peso) * agilidad)/10;
+        }
+        public double Saltar()
+        {
+            double altura;
+            return altura = ((energia / peso) * resistencia)/10;
+        }
+        public double Atacar()
+        {
+            double daño;
+            return daño = (energia * peso * agilidad + (iq))/1000;
+        }
+    }
+
+    public class Perro : Ser_Vivo
+    {
+        //Atributos
+        public double mordedura { get; set; }
+
+        //Constructor
+        public Perro(double peso, int energia, double resistencia, int agilidad, int iq, double mordedura) : base(peso, energia, resistencia, agilidad, iq)
+        {
+            this.mordedura = mordedura;
+        }
+
+        //Metodos
+        public override void Comunicar()
+        {
+            Console.WriteLine("El perro ladra");
+        }
+        public void Ladrar()
+        {
+            double volumen = energia * agilidad * iq + (mordedura);
+            System.Console.WriteLine("El perro ladra con un volumen de: " + volumen);
+        }
+    }
+
+    public class Gato : Ser_Vivo
+    {
+        //Atributos
+        public double sigilo { get; set; }
+        //Constructor
+        public Gato(double peso, int energia, double resistencia, int agilidad, int iq, double sigilo) : base(peso, energia, resistencia, agilidad, iq)
+        {
+            this.sigilo = sigilo;
+        }
+        //Metodos
+        public override void Comunicar()
+        {
+            Console.WriteLine("El gato maúlla");
+        }
+        public void Maullar()
+        {
+            double volumen = energia * agilidad * iq + (sigilo);
+            System.Console.WriteLine("El gato maulla con un volumen de: " + volumen);
+        }
+    }
+
+    public class Humano : Ser_Vivo
+    {
+        //Atributos
+        public double fuerza { get; set; }
+        //Constructor
+        public Humano(double peso, int energia, double resistencia, int agilidad, int iq, double fuerza) : base(peso, energia, resistencia, agilidad, iq)
+        {
+            this.fuerza = fuerza;
+        }
+        //Metodos
+        public override void Comunicar()
+        {
+            Console.WriteLine("El humano hace sonidos extraños.");
+        }
+        public void Hablar()
+        {
+            double volumen = energia * agilidad * iq + (fuerza);
+            System.Console.WriteLine("El humano habla con un volumen de: " + volumen);
+        }
+    }
+}
